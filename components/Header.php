30691754baa934e654c0ca2e4a77104f3175ddfe
mirkobechini <mirkobechini@gmail.com>
2025-12-17 09:17:32 +0100

    Update Header component to use icon for branding and include Bootstrap Icons stylesheet in index.php

--- components/Header.php
+++ components/Header.php
@@ -2,7 +2,7 @@
     <nav class="navbar bg-dark p-1">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="" alt="Movies" width="24">
+                <i class="bi bi-film text-light">&nbsp;Movie</i>
             </a>
         </div>
     </nav>
--- index.php
+++ index.php
@@ -15,6 +15,7 @@ require_once "./data/db.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 
 <body>
